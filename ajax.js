5ee1b9c887d7b8ef33d2b4fbc94074f2084f6e09
Natasha <njohnson533@gmail.com>
2021-11-15 15:10:08 -0500

    added getWeatherCelcius and getWeatherFahrenheit functions

--- ajax.js
+++ ajax.js
@@ -1,42 +1,104 @@
-
 const btn = document.querySelector(".btn");
-// const URL =
-	"http://api.openweathermap.org/data/2.5/weather?q=Richmond&APPID=e3a62ee996e2dfe8059942823c005091&units=imperial";
 
-function getWeather(e) {
-    e.preventDefault();
-    // get input from user
-    const location = document.querySelector(".location").value;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=e3a62ee996e2dfe8059942823c005091&units=imperial`;
-    const https = new XMLHttpRequest();
-    https.open(
-			"GET",
-			url,
-			true
-		);
-    https.onload = function() {
-        if (this.status === 200) {
-            const response = JSON.parse(this.responseText);
-            let output = "";
-            let weather = response.weather[0].main;
-            let description = response.weather[0].description;
-            let icon = response.weather[0].icon;
-            let temp = Math.round(response.main.temp);
-            let city = response.name;
-            console.log(temp);
-            let isrc = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-            output += `
-                <h2>Weather</h2>
-                <p>${weather}</p>
-                <p>${description}</p>
-                <p>${temp}&deg;F</p>
-                <p>${city}</p>
-            `;
-            document.querySelector(".weather").innerHTML = output;
-            document.getElementById("logo").src = isrc;
-        }
-    }
-    https.send();
+function getLocation() {
+    const loc = document.querySelector(".location").value;
+    return loc;
 }
 
-btn.addEventListener("click", getWeather);
+function getWeatherFahrenheit() {
+
+	const unitType = "imperial";
+	const location = getLocation();
+
+	const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=e3a62ee996e2dfe8059942823c005091&units=${unitType}`;
+	const https = new XMLHttpRequest();
+	https.open("GET", url, true);
+	https.onload = function () {
+		if (this.status === 200) {
+			const response = JSON.parse(this.responseText);
+			let output = "";
+			let description = response.weather[0].description;
+			let icon = response.weather[0].icon;
+			let temp = Math.round(response.main.temp);
+			let city = response.name;
+			let date = new Date();
+			console.log(temp);
+			let isrc = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+			output += `
+                <div class="buttons">
+                    <button class="deg active" id="fahrenheit">F&deg;</button>
+                    <button class="deg" id="celcius">C&deg;</button>
+                </div>
+                <h2>Weather <small>as of ${date.toLocaleTimeString()}</small></h2>
+                <p class="description">${description}</p>
+                <p class="temp">${temp}&deg;F</p>
+                <p>${city}</p>
+            `;
+			document.querySelector(".weather").innerHTML = output;
+			document.getElementById("logo").src = isrc;
+			const fahrenheit = document.getElementById("fahrenheit");
+			const celcius = document.getElementById("celcius");
+			fahrenheit.addEventListener("click", getWeatherFahrenheit);
+			celcius.addEventListener("click", getWeatherCelcius);
+		} else if (this.status === 404) {
+			document.querySelector(".weather").textContent =
+				"Unable to get weather for this location, please try another location!";
+			document.querySelector(".location").value = "";
+		}
+        //reset input
+        // document.querySelector(".location").value = "";
+	};
+	https.send();
+}
+
+function getWeatherCelcius() {
+    fahrenheit.classList.remove("active");
+	celcius.classList.add("active");
+
+    const unitType = "metric";
+    const location = getLocation();
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=e3a62ee996e2dfe8059942823c005091&units=${unitType}`;
+		const https = new XMLHttpRequest();
+		https.open("GET", url, true);
+		https.onload = function () {
+			if (this.status === 200) {
+				const response = JSON.parse(this.responseText);
+				let output = "";
+				let description = response.weather[0].description;
+				let icon = response.weather[0].icon;
+				let temp = Math.round(response.main.temp);
+				let city = response.name;
+				let date = new Date();
+				console.log(temp);
+				let isrc = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+				output += `
+                <div class="buttons">
+                    <button class="deg" id="fahrenheit">F&deg;</button>
+                    <button class="deg" id="celcius">C&deg;</button>
+                </div>
+                <h2>Weather <small>as of ${date.toLocaleTimeString()}</small></h2>
+                <p class="description">${description}</p>
+                <p class="temp">${temp}&deg;C</p>
+                <p>${city}</p>
+            `;
+				document.querySelector(".weather").innerHTML = output;
+				document.getElementById("logo").src = isrc;
+				const fahrenheit = document.getElementById("fahrenheit");
+				const celcius = document.getElementById("celcius");
+				celcius.classList.add("active");
+				fahrenheit.classList.remove("active");
+				fahrenheit.addEventListener("click", getWeatherFahrenheit);
+				celcius.addEventListener("click", getWeatherCelcius);
+			} else if (this.status === 404) {
+				document.querySelector(".weather").textContent =
+					"Unable to get weather for this location!";
+				document.querySelector(".location").value = "";
+			}
+			//reset input
+			// document.querySelector(".location").value = "";
+		};
+		https.send();
+}
+
+btn.addEventListener("click", getWeatherFahrenheit);
