ab1207d1d3e9e93933e5843d78ef8c9045cc2e65
Natasha <njohnson533@gmail.com>
2021-11-15 22:54:42 -0500

    added currentWeather function and styling

--- app.js
+++ app.js
@@ -1,32 +1,98 @@
-
 const btn = document.querySelector(".btn");
+const weekdays = ["Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"];
+const months = [
+	"Jan",
+	"Feb",
+	"Mar",
+	"Apr",
+	"May",
+	"Jun",
+	"Jul",
+	"Aug",
+	"Sept",
+	"Oct",
+	"Nov",
+	"Dec",
+];
 
-
-const URL =
-	"http://api.openweathermap.org/data/2.5/weather?q=Richmond&APPID=e3a62ee996e2dfe8059942823c005091&units=imperial";
-
-// window.addEventListener("DOMContentLoaded", () => {
-//     showWeather(URL);
-// })
-
-const showWeather = async (URL) => {
-    // const data = await fetchWeather(url);
-    const img = document.getElementById("logo");
-    
+const currentWeather = async (loc) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=e3a62ee996e2dfe8059942823c005091&units=imperial`;
     try {
-        const response = await fetch(URL);
+        const response = await fetch(url);
         const data = await response.json();
         console.log(data);
-        const { temp, temp_max: max, temp_min: min } = data.main;
-        //const { icon } = data.weather;
-        console.log(data.weather[0].icon, temp, max, min);
-        //img.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-
-        console.log(img.src);
-        return data;
+        const { temp } = data.main;
+        const { main, icon } = data.weather[0];
+        const { name } = data;
+        let date = new Date();
+        const weekday = weekdays[date.getDay()];
+        const month = months[date.getMonth()];
+        const day = date.getDate();
+        let output = "";
+        output += `
+            <h1 class="city">${name}</h1>
+            <small>Weather as of ${date.toLocaleTimeString()}</small>
+            <p class="description">${main}</p>
+            <p class="temp">${Math.round(temp)}&deg;F</p>
+            <img src="http://openweathermap.org/img/wn/${icon}@2x.png"></img>
+            <p>${weekday}, ${month} ${day}</p>
+        `;
+        document.querySelector(".current").innerHTML = output;
     } catch (error) {
-        console.log(error);
+        document.querySelector(".current").innerHTML =
+            "Unable to get weather for this location, please try another location!";
+        document.querySelector(".weather").innerHTML = "";
     }
+    document.querySelector(".location").value = "";
 }
 
-btn.addEventListener("click", showWeather);
+const showWeather = async (url) => {
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        let output = "";
+        for (let i = 5; i <data.list.length; i+=8){
+            const { temp } = data.list[i].main;
+            const { main, icon } = data.list[i].weather[0];
+            const { name } = data.city;
+            const { dt_txt } = data.list[i];
+            const date = new Date(dt_txt);
+            const weekday = weekdays[date.getDay()];
+            const month = months[date.getMonth()];
+            const day = date.getDate();
+            output += `
+            <div class="card">
+                <p class="description">${main}</p>
+                <p class="temp">${Math.round(temp)}&deg;F</p>
+                <img src="http://openweathermap.org/img/wn/${icon}@2x.png"></img>
+                <p>${weekday}, ${month} ${day}</p>
+            </div>
+            `;
+            
+        }
+        document.querySelector(".weather").innerHTML = output;
+    } catch (error) {
+        document.querySelector(".current").innerHTML = "Unable to get weather for this location, please try another location!";
+        document.querySelector(".weather").innerHTML = "";
+    }
+    document.querySelector(".location").value = "";
+}
+
+btn.addEventListener("click", () => {
+    const loc = document.querySelector(".location").value;
+    const error = document.querySelector(".error");
+    const regex = /[^$|\d|\s{2}*]/ig;
+    if (loc.match(/^$/) || loc.match(/\d/) || loc.match(/\s{2,}/)) {
+        error.textContent = "Enter a valid city!";
+        setTimeout(() => {
+            error.textContent = "";
+        }, 3000);
+        return;
+        
+    }
+    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${loc}&appid=e3a62ee996e2dfe8059942823c005091&units=imperial`;
+    
+    showWeather(url);
+    currentWeather(loc);
+});
